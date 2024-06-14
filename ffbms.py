cff2474cb9ae5807803bc30818070a3e186a2213
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-06-14 21:53:11 +0200

    added ability to start script without args, default values will be used instead

--- ffbms.py
+++ ffbms.py
@@ -3,8 +3,12 @@
 import os
 from sys import argv
 
-target_file = argv[1]
-target_sites = argv[2:]
+if len(argv) > 1:
+    target_file = argv[1]
+    target_sites = argv[2:]
+else:
+    target_file = 'bookmarks.html'
+    target_sites = []
 
 # clean the console. This is neccessary for ASCII color to work in Windows CMD.
 if os.name == 'nt':
